54507e37c97353ca63faea9dc2e8c2b87641d004
rkhademul8 <rkhademul8.com>
2022-03-24 08:47:54 +0600

    carousels_addefd
    clear

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Carousel, Container, Row } from 'react-bootstrap';
 import Cocktails from '../Cocktails/Cocktails';
+import './Home.css'
+
+import img1 from '../../images/m1.jpg'
+import img2 from '../../images/m2.jpg'
+import img3 from '../../images/m4.jpg'
 
 const Home = () => {
 
@@ -15,8 +20,52 @@ const Home = () => {
     return (
         <div>
 
-            <Container>
+                {/* carousels  */}
 
+                <Container className='my-5'>
+                        <Carousel>
+
+                            <Carousel.Item className='carousels'>
+                                <img 
+                                className="d-block w-100"
+                                src={img1}
+                                alt="First slide"
+                                />
+                                <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+
+                            <Carousel.Item className='carousels'>
+                                <img 
+                                className="d-block w-100"
+                                src={img2}
+                                alt="Second slide"
+                                />
+
+                                <Carousel.Caption>
+                                <h3>Second slide label</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item className='carousels'>
+                                <img 
+                                className="d-block w-100"
+                                src={img3}
+                                alt="Third slide"
+                                />
+
+                                <Carousel.Caption>
+                                <h3>Third slide label</h3>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel>
+                </Container>
+
+                {/* cocktail map from api */}
+            <Container>
                 <Row xs={1} md={3} className="g-4">
                     {
                         cocktails.map(cocktail=> <Cocktails
@@ -25,7 +74,6 @@ const Home = () => {
                         ></Cocktails>)
                     }
                 </Row>
-
             </Container>
            
         </div>
